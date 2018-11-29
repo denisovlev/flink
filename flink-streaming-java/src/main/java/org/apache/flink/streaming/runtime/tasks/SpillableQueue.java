3b9803a2ae02fdfda79f0f45e4cee9d3b60f41a5
Lev Denisov <denisovlev@ya.ru>
2018-11-30 00:01:50 +0100

    Implement HALF algorithm for queue spilling to disk http://ilpubs.stanford.edu:8090/618/1/2003-63.pdf

---
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/tasks/SpillableQueue.java
@@ -0,0 +1,176 @@
+package org.apache.flink.streaming.runtime.tasks;
+
+import org.apache.flink.api.common.typeutils.base.IntSerializer;
+import org.apache.flink.core.memory.DataInputView;
+import org.apache.flink.core.memory.DataInputViewStreamWrapper;
+import org.apache.flink.core.memory.DataOutputView;
+import org.apache.flink.core.memory.DataOutputViewStreamWrapper;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class SpillableQueue<E> {
+
+	public static void main(String[] args) throws Exception {
+		SpillableQueue<Integer> q = new SpillableQueue<Integer>(5, new WrapSerializer(new IntSerializer()));
+		int begin = 10;
+		int count = 40;
+		for (int i = begin; i < begin + count; i++) {
+			System.out.println("\u001B[32m" + "Put: " + i + "\u001B[0m");
+			q.put(i);
+		}
+		for (int i = 0; i < count; i++) {
+			System.out.println("\u001B[31m" + "Take: " + q.take() + "\u001B[0m");
+		}
+	}
+
+	private BackDrainableQueue<E> head;
+	private BackDrainableQueue<E> tail;
+	private LinkedBlockingQueue<SpilledBuffer<E>> spilled;
+
+	private int M;
+
+	private ItemSerializer<E> serializer;
+
+	SpillableQueue(Integer M, ItemSerializer<E> serializer) {
+		this.M = M;
+		this.serializer = serializer;
+		head = new BackDrainableQueue<E>(2*M);
+		tail = new BackDrainableQueue<E>(M);
+		spilled = new LinkedBlockingQueue<>();
+	}
+
+	void put(E item) throws InterruptedException, IOException {
+		BackDrainableQueue<E> cur;
+		if (spilled.isEmpty()) {
+			cur = head;
+		} else {
+			cur = tail;
+		}
+		if(cur.remainingCapacity() == 0) {
+			System.out.println("Write-OUT");
+		  	SpilledBuffer<E> buf = new SpilledBuffer<E>(M, serializer);
+		  	cur.drainBackTo(buf.getRecords(), M);
+		  	buf.writeOut();
+		  	spilled.put(buf);
+		  	System.out.println("Space left:" + cur.remainingCapacity());
+		  	cur = tail;
+		}
+		cur.put(item);
+	}
+
+	E take() throws InterruptedException, IOException {
+		if (head.isEmpty()) {
+			if (!spilled.isEmpty()) {
+				System.out.println("Read-IN");
+				SpilledBuffer<E> buf = spilled.take();
+				buf.readIn();
+				System.out.println("Reading from spill: " + buf.getRecords());
+				head.addAll(buf.getRecords());
+			}
+			if (spilled.isEmpty()) {
+				System.out.println("Transfer");
+				tail.drainTo(head);
+			}
+		}
+		E item = head.take();
+		return item;
+	}
+}
+
+class BackDrainableQueue<E> extends ArrayBlockingQueue<E>{
+
+	public BackDrainableQueue(int capacity) {
+		super(capacity);
+	}
+
+	int drainBackTo(Collection<? super E> c, int maxElements) {
+		Iterator<E> it = this.iterator();
+		int queueSize = this.size();
+		int toSkip = queueSize - maxElements;
+		int removed = 0;
+		System.out.println("Queue size: " + queueSize + " cap: " + this.remainingCapacity() + " Skip size: " + toSkip + " maxElements: " + maxElements);
+		for (int i = 0; i < queueSize; i++) {
+			E item = it.next();
+			if (i < toSkip) {
+				System.out.println("Iter: " + i + " Skipped: " + item);
+				continue;
+			}
+			c.add(item);
+			it.remove();
+			System.out.println("Iter: " + i + " Drained: " + item);
+			removed++;
+		}
+		return removed;
+	}
+}
+
+interface ItemSerializer<E> {
+	public void serialize(E value, DataOutputView target) throws IOException;
+	public E deserialize(DataInputView source) throws IOException;
+}
+
+class WrapSerializer implements ItemSerializer<Integer> {
+
+	private IntSerializer ser;
+
+	WrapSerializer(IntSerializer ser) {
+		this.ser = ser;
+	}
+
+	@Override
+	public void serialize(Integer value, DataOutputView target) throws IOException {
+		ser.serialize(value, target);
+	}
+
+	@Override
+	public Integer deserialize(DataInputView source) throws IOException {
+		return ser.deserialize(source);
+	}
+}
+
+class SpilledBuffer<E> {
+
+	private int size;
+	private ItemSerializer<E> serializer;
+	private ArrayList<E> records;
+	File tempFile;
+	DataOutputViewStreamWrapper out;
+	DataInputViewStreamWrapper in;
+
+	SpilledBuffer(int size, ItemSerializer<E> serializer) throws IOException {
+		this.size = size;
+		this.serializer = serializer;
+		records = new ArrayList<E>(size);
+
+		tempFile = File.createTempFile("spill-", ".tmp");
+		tempFile.deleteOnExit();
+		out = new DataOutputViewStreamWrapper(new BufferedOutputStream(new FileOutputStream(tempFile)));
+		in = new DataInputViewStreamWrapper(new BufferedInputStream(new FileInputStream(tempFile)));
+	}
+
+	void writeOut() throws IOException {
+		System.out.println("Writing to disk: " + records);
+		for (E record: records) {
+			serializer.serialize(record, out);
+		}
+		out.flush();
+		records.clear();
+	}
+
+	void readIn() throws IOException {
+		for (int i = 0; i < size; i++) {
+			E record = serializer.deserialize(in);
+			records.add(record);
+		}
+		System.out.println("Read from disk: " + records);
+	}
+
+	ArrayList<E> getRecords() {
+		return records;
+	}
+}
