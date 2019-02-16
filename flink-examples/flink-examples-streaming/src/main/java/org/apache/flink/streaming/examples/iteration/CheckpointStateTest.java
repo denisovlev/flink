5f3efbecd90ca92bb0ebb5a9cd91d98641ca59e4
tiffany <joan.tiffany.ong.lopez@ulb.ac.be>
2019-02-16 16:34:29 +0100

    Add lock in source; use ListCheckpointed

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/iteration/CheckpointStateTest.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/iteration/CheckpointStateTest.java
@@ -1,23 +1,20 @@
 package org.apache.flink.streaming.examples.iteration;
 
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.state.ListState;
-import org.apache.flink.api.common.state.ListStateDescriptor;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.runtime.state.FunctionInitializationContext;
-import org.apache.flink.runtime.state.FunctionSnapshotContext;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
-import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.streaming.api.checkpoint.ListCheckpointed;
 import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.IterativeStream;
 import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -63,16 +60,19 @@ public class CheckpointStateTest {
 	/**
 	 * Tuple2(number, boolean if number has entered the iteration body before)
 	 */
-	private class NumberSource implements SourceFunction<Tuple2<Long, Boolean>>, CheckpointedFunction {
+	private class NumberSource extends RichParallelSourceFunction<Tuple2<Long, Boolean>> implements ListCheckpointed<Long> {
 		private boolean isRunning = true;
 		private long number = 0;
-		private transient ListState<Long> checkpointedNumber;
 
 		@Override
 		public void run(SourceContext<Tuple2<Long, Boolean>> ctx) throws Exception {
+			final Object lock = ctx.getCheckpointLock();
+
 			while (isRunning) {
 				if (number <= 10000) {
-					ctx.collect(new Tuple2<Long, Boolean>(number++, false));
+					synchronized (lock) {
+						ctx.collect(new Tuple2<Long, Boolean>(number++, false));
+					}
 				}
 
 				Thread.sleep(1);
@@ -91,29 +91,22 @@ public class CheckpointStateTest {
 		}
 
 		@Override
-		public void snapshotState(FunctionSnapshotContext context) throws Exception {
-			this.checkpointedNumber.clear();
-			this.checkpointedNumber.add(number);
-			LOG.debug("NumberSource save tuple={}", this.number);
+		public List<Long> snapshotState(long checkpointId, long timestamp) throws Exception {
+			LOG.debug("NumberSource save tuple={}", number);
+			return Collections.singletonList(number);
 		}
 
 		@Override
-		public void initializeState(FunctionInitializationContext context) throws Exception {
-			this.checkpointedNumber = context.getOperatorStateStore()
-				.getListState(new ListStateDescriptor<>("NumberSource count", Long.class));
-
-			if (context.isRestored()) {
-				for (Long item : this.checkpointedNumber.get()) {
-					this.number = item;
-					LOG.debug("NumberSource load tuple={}", this.number);
-				}
+		public void restoreState(List<Long> state) throws Exception {
+			for (Long s : state) {
+				number = s;
+				LOG.debug("NumberSource load tuple={}", number);
 			}
 		}
 	}
 
-	private class ChecksumChecker implements MapFunction<Tuple2<Long, Boolean>, Tuple2<Long, Boolean>>, CheckpointedFunction {
+	private class ChecksumChecker implements MapFunction<Tuple2<Long, Boolean>, Tuple2<Long, Boolean>>, ListCheckpointed<Long> {
 		private long sum = 0;
-		private transient ListState<Long> checkpointedSum;
 
 		@Override
 		public Tuple2<Long, Boolean> map(Tuple2<Long, Boolean> tuple) throws Exception {
@@ -134,23 +127,18 @@ public class CheckpointStateTest {
 		}
 
 		@Override
-		public void snapshotState(FunctionSnapshotContext context) throws Exception {
-			this.checkpointedSum.clear();
-			this.checkpointedSum.add(sum);
-			LOG.debug("ChecksumChecker save tuple={}", this.sum);
+		public List<Long> snapshotState(long checkpointId, long timestamp) throws Exception {
+			LOG.debug("ChecksumChecker save tuple={}", sum);
+			return Collections.singletonList(sum);
 		}
 
 		@Override
-		public void initializeState(FunctionInitializationContext context) throws Exception {
-			this.checkpointedSum = context.getOperatorStateStore()
-				.getListState(new ListStateDescriptor<>("ChecksumChecker count", Long.class));
-
-			if (context.isRestored()) {
-				for (Long item : this.checkpointedSum.get()) {
-					this.sum = item;
-					LOG.debug("ChecksumChecker load tuple={}", this.sum);
-				}
+		public void restoreState(List<Long> state) throws Exception {
+			for (Long s : state) {
+				sum = s;
+				LOG.debug("ChecksumChecker load tuple={}", sum);
 			}
 		}
 	}
+
 }
